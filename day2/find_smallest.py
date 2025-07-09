a8987e6436a10985bdb6d80ddf97ce6f61d1de62
divs111 <divyasoni01@icloud.com>
2025-07-09 14:22:09 +0530

    modified

--- day2/find_smallest.py
+++ day2/find_smallest.py
@@ -3,21 +3,20 @@
 #Find the smallest and Second Smallest Numbers from that list
 
 
-num=int(input("enter a num:"))
+num = int(input("how many numbers you want to enter: "))
 list=[]
 for i in range(num):
-    n=int(input(f"Enter a number {i + 1}:"))
+    n=int(input(f"Enter a number{i +1} : "))
     list.append(n)
 print(list)
-
 list.sort()
 print(list)
+
 smallest=list[0]
 second_sm=None
-for number in list:
-    if number > smallest:
-        second_sm = number
+for small in list:
+    if small > smallest:
+        second_sm=small
         break
-print("smallest number: ",smallest)
-print("second smallest: ",second_sm)
-
+#print("smallest number is:",smallest ,"and second smallest is :",second_sm)
+print(f"Smallest number is : {smallest} Second smallest is {second_sm}")
